4dcc734270b23b4bc4fa1a57b098fe87aa6bc849
XHXYT <xytdg520@qq.com>
2025-08-23 12:02:08 +0800

    修复  图表持续绘制导致的界面错乱问题(待验证)  主页IP显示问题(切换刷新时受缓存影响仍然存在Unknown)

--- proxy_core/src/main/ets/Request.ts
+++ proxy_core/src/main/ets/Request.ts
@@ -11,7 +11,13 @@ const IpCountryList: IpResolver[] = [{
 }]
 const ipInfoSources: IpResolver[]  = [
   {
-    url: "https://ipwho.is/?fields=ip&output=csv",
+    url: "https://api.vore.top/api/IPdata?ip=",
+    resolve: (json, text)=>{
+      return json["ipinfo"]["text"] as string
+    }
+  },
+  {
+    url: "https://ipwho.is/",
     resolve: (json, text)=>{
       return json["ip"] as string
     }
@@ -34,9 +40,11 @@ const ipInfoSources: IpResolver[]  = [
 export async function CallIpResolver(ip: string | undefined, resolver: IpResolver | string): Promise<string | null>{
   let httpRequest = http.createHttp()
   const url = typeof resolver == "string" ? resolver as string : resolver.url
+  console.log(`IPtest #CallIpResolver 即将请求的原始链接: ${url}`)
   let json = null
   try {
-    const resp = await httpRequest.request(url + ip ?? "", {connectTimeout: 5000, readTimeout: 2000})
+    console.log(`IPtest #CallIpResolver 即将请求的ip: ${ip}, 链接: ${url + (ip != undefined ? ip : '')}`)
+    const resp = await httpRequest.request(url + (ip ?? ''), {connectTimeout: 5000, readTimeout: 2000})
     if(resp.responseCode !== 200)
       return null;
     if (typeof resolver == "string" ){
@@ -52,11 +60,14 @@ export async function CallIpResolver(ip: string | undefined, resolver: IpResolve
     console.error("CallIpResolver error: ", url, e.message, JSON.stringify(e))
     httpRequest.destroy()
     return null
+  } finally {
+    httpRequest.destroy()
   }
 }
 export async function queryIpInfo(ip: string){
   for (let resolver of IpCountryList) {
     const result = await CallIpResolver(ip, resolver)
+    console.log(`IPtest #queryIpInfo result: ${result}`)
     if (!result)
       continue
     return result
@@ -65,7 +76,9 @@ export async function queryIpInfo(ip: string){
 }
 export async function checkIp() {
   for (let source of ipInfoSources) {
+    console.log(`IPtest #checkIp source: ${JSON.stringify(source)}`)
     const result = await CallIpResolver(undefined, source)
+    console.log(`IPtest #checkIp result: ${result}`)
     if (!result || result == "")
       continue
     return result
